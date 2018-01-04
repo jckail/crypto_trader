2bedd5077f72f6dcb924f8064ddcd4c30ddfb83b
Jordan <jckail@users.noreply.github.com>
2018-01-04 12:35:40 -0700

    fixed file output to s3 and json dumbassness works in athena hopefully fixes odd table issue

--- alpha/balpha_runner.sh
+++ alpha/balpha_runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ~/anaconda3/bin/activate
 #for org in Y #input many facilites
 #do # execute per loop? pass per loop?
 ./mt_alpha_runner.py --run 'Y' --runfocus_symbols_only 'Y' --runisprice 'N'
