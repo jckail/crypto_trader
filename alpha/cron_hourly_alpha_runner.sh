39ac81591333ff9865f1c9b94ca4268bea5f9de3
Jordan <jckail@users.noreply.github.com>
2018-01-05 12:25:32 -0700

    cronfix

--- alpha/cron_hourly_alpha_runner.sh
+++ alpha/cron_hourly_alpha_runner.sh
@@ -3,5 +3,5 @@ source ~/anaconda3/bin/activate
 cd /home/ubuntu/lit_crypto/alpha
 #for org in Y #input many facilites
 #do # execute per loop? pass per loop?
-./mt_alpha_runner.py --run 'Y' --runfocus_symbols_only 'Y' --runcrawler 'N'
+./mt_alpha_runner.py --run 'Y' --runfocus_symbols_only 'N' --runcrawler 'N'
 #done #for for loop
