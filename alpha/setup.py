77e04b4158d474d54f7ca0869bd6877aa3916e98
Jordan <jckail@users.noreply.github.com>
2018-01-06 10:32:15 -0700

    tested athena quicksight ran into window function isssue
    ran into window function in quicksight issue code correct

--- alpha/setup.py
+++ alpha/setup.py
@@ -1,4 +1,5 @@
 import os
+
 cwd = os.getcwd()
 from time import sleep
 from tqdm import tqdm
@@ -11,55 +12,53 @@ import logging
 
 class Setup(object):
 
-    def __init__(self,cwd):
+    def __init__(self, cwd):
         self.cwd = cwd
-
 
     def build_data_path(self):
 
-
         cwd_split = self.cwd.split('/')
 
-        target_ibdex = cwd_split.index('alpha') # project name
-        self.cwd = '/'.join(cwd_split[:target_ibdex-1])+'/lit_crypto_data/alpha'
+        target_ibdex = cwd_split.index('alpha')  # project name
+        self.cwd = '/'.join(cwd_split[:target_ibdex - 1]) + '/lit_crypto_data/alpha'
 
         if not os.path.exists(self.cwd):
             os.makedirs(self.cwd)
-            print ('Created: '+self.cwd)
+            print('Created: ' + self.cwd)
         return self.cwd
 
     def validate_directories(self):
         try:
-            create_list = ['/data/'\
-                ,'/data/coinmarketcap/'\
-                ,'/data/day_data/'\
-                ,'/data/hour_data/'\
-                ,'/data/mining_data/'\
-                ,'/data/minute_data/'\
-                ,'/data/social/' \
-                ,'/data/social/reddit/' \
-                ,'/data/social/twitter/' \
-                ,'/data/social/coderepository/' \
-                ,'/data/social/facebook/' \
-                ,'/data/social/cryptocompare/' \
-                ,'/data/social/general/' \
-                ,'/data/trading_pair/'\
-                ,'/data/coininfo/'\
-                ,'/data/pricedetails/'\
-                ,'/data/mining_data/coin_miner_data/'\
-                ,'/data/mining_data/miner_data/'
+            create_list = ['/data/' \
+                , '/data/coinmarketcap/' \
+                , '/data/day_data/' \
+                , '/data/hour_data/' \
+                , '/data/mining_data/' \
+                , '/data/minute_data/' \
+                , '/data/social/' \
+                , '/data/social/reddit/' \
+                , '/data/social/twitter/' \
+                , '/data/social/coderepository/' \
+                , '/data/social/facebook/' \
+                , '/data/social/cryptocompare/' \
+                , '/data/social/general/' \
+                , '/data/trading_pair/' \
+                , '/data/coininfo/' \
+                , '/data/pricedetails/' \
+                , '/data/mining_data/coin_miner_data/' \
+                , '/data/mining_data/miner_data/'
                            ]
 
-            #for y in tqdm(create_list,desc='validate_directories'): #progressbar
+            # for y in tqdm(create_list,desc='validate_directories'): #progressbar
             for y in create_list:
-                directory = self.cwd+y
-                gdirectory = self.cwd+y+"gzip_files/"
+                directory = self.cwd + y
+                gdirectory = self.cwd + y + "gzip_files/"
                 if not os.path.exists(directory):
                     os.makedirs(directory)
-                    print ('Created: '+directory)
+                    print('Created: ' + directory)
                 if not os.path.exists(gdirectory):
                     os.makedirs(gdirectory)
-                    print ('Created: '+gdirectory)
+                    print('Created: ' + gdirectory)
         except Exception as e:
             logging.info('------')
             logging.error(traceback.format_exc())
@@ -68,17 +67,15 @@ class Setup(object):
             logging.info('------')
             print(e)
 
-
-
     def main(self):
         try:
             iam = boto3.resource('iam')
             current_user = iam.CurrentUser()
-            print('Current User: '+current_user.user_name)
+            print('Current User: ' + current_user.user_name)
             s = Setup(self.cwd)
             self.cwd = s.build_data_path()
             s.validate_directories()
-            #print(self.cwd)
+            # print(self.cwd)
             return self.cwd
         except Exception as e:
             logging.info('------')
@@ -88,10 +85,9 @@ class Setup(object):
             logging.info('------')
             print(e)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     cwd = os.getcwd()
 
     s = Setup()
     s.main()
-
